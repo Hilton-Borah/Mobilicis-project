eb045728a76093e479c1d8a3932aa4694f52cc06
Hilton-Borah <hiltonborah123@gmail.com>
2023-08-16 19:38:24 +0530

    alldone

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -10,26 +10,17 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "//app.snipcart.com/api/(.*)",
+        // matching all API routes
+        source: "/api/login",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://www.swindon-meats.co.uk",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
